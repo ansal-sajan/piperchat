fb85c2aed04c1134f81caebf0a442750de1607da
Ansal Sajan <ansalsajan1235@gmail.com>
2024-03-08 16:39:27 +0000

    added

---
+++ src/main/java/com/app/piperchat/PiperchatApplication.java
@@ -0,0 +1,13 @@
+package com.app.piperchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiperchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiperchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/piperchat/PiperchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.piperchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiperchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
